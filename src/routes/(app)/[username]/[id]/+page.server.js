3b7d519424a6ca4028bd0a6ad1907f5f228a4963
Scott Cook <cookmscott@gmail.com>
2025-10-29 19:03:29 -0400

    single image page does its own load from db
    profile and now single image page loads details from db .. needs love but is hooked up now

---
+++ src/routes/(app)/[username]/[id]/+page.server.js
@@ -0,0 +1,30 @@
+import { error } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params, locals: { supabase } }) {
+	const { data: photo, error: photoError } = await supabase
+		.from('photos')
+		.select(
+			`
+			*,
+			user:user_profiles (
+				username,
+				display_name,
+				avatar_url
+			)
+		`
+		)
+		.eq('id', params.id)
+		.single();
+
+	if (photoError || !photo) {
+		error(404, 'Not found');
+	}
+
+	return {
+		photo,
+		streamed: {
+			// You can add other streamed data here later, like comments or related photos.
+		}
+	};
+}
